7caed62ff3fa6f98341659f77ab356108e073b5f
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-12-28 21:46:28 -0500

    update ads, index

--- src/database/ads.js
+++ src/database/ads.js
@@ -1,4 +1,5 @@
 const {getDatabase} = require('./mongo');
+const {ObjectId} = require('mongodb');
 
 const collectionName = 'ads';
 
@@ -13,7 +14,30 @@ async function getAds() {
   return await database.collection(collectionName).find({}).toArray();
 }
 
+async function deleteAd(id) {
+  const database = await getDatabase();
+  await database.collection(collectionName).deleteOne({
+    _id: new ObjectId(id),
+  });
+}
+
+async function updateAd(id, ad) {
+  const database = await getDatabase();
+  delete ad._id;
+  await database.collection(collectionName).update(
+    { _id: new ObjectId(id), },
+    {
+      $set: {
+        ...ad,
+      },
+    },
+  );
+}
+
+
 module.exports = {
   insertAd,
   getAds,
+  deleteAd,
+  updateAd,
 };
--- src/index.js
+++ src/index.js
@@ -27,10 +27,30 @@ app.use(morgan('combined'));
 
 const {startDatabase} = require('./database/mongo');
 const {insertAd, getAds} = require('./database/ads');
+const {deleteAd, updateAd} = require('./database/ads');
 
 // defining an endpoint to return all ads
 app.get('/', async (req, res) => {
     res.send(await getAds());
+});
+
+app.post('/', async (req, res) => {
+    const newAd = req.body;
+    await insertAd(newAd);
+    res.send({ message: 'New ad inserted.' });
+});
+  
+  // endpoint to delete an ad
+app.delete('/:id', async (req, res) => {
+    await deleteAd(req.params.id);
+    res.send({ message: 'Ad removed.' });
+});
+  
+  // endpoint to update an ad
+app.put('/:id', async (req, res) => {
+    const updatedAd = req.body;
+    await updateAd(req.params.id, updatedAd);
+    res.send({ message: 'Ad updated.' });
 });
 
 // start the in-memory MongoDB instance
